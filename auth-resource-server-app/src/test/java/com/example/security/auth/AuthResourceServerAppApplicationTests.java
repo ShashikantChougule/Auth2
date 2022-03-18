87c0ccf9d35e943055b8b229d8efeb80dd66c650
Shashikant_Chougule <shashichougule6446@gmail.com>
2022-03-18 10:51:12 +0530

    Auth2

---
+++ auth-resource-server-app/src/test/java/com/example/security/auth/AuthResourceServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthResourceServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
